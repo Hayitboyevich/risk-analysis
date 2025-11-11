10187e99654dd2a07ab6f10b7c05d3fee1abc6d5
Hayitboyevich <shhayitboyevich@gmail.com>
2025-11-11 11:19:35 +0500

    asdfasdf

--- app/Models/IllegalObjectQuestion.php
+++ app/Models/IllegalObjectQuestion.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class IllegalObjectQuestion extends Model
 {
-    //
+    protected $guarded = false;
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(IllegalQuestionType::class, 'illegal_question_type_id');
+    }
 }
