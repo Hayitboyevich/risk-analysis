3ce9d0f48ca5546843f4425bfac69c18164f57c3
Hayitboyevich <shhayitboyevich@gmail.com>
2025-11-11 11:52:03 +0500

    bug fix

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Document extends Model
 {
-    //
+    protected $guarded = false;
+
+    public function documentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
--- app/Models/IllegalObjectCheckListHistory.php
+++ app/Models/IllegalObjectCheckListHistory.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class IllegalObjectCheckListHistory extends Model
 {
-    //
+    protected $casts = [
+        'content' => 'array'
+    ];
+
+    public function documents(): MorphMany
+    {
+        return $this->morphMany(Document::class, 'documentable');
+    }
 }
