9dba3dae5541a0ad6d2dad148765978ab4f8527c
Hayitboyevich <shhayitboyevich@gmail.com>
2025-11-24 15:22:31 +0500

    sfdfgsdfg

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Region extends Model
 {
-    //
+    protected  $guarded = false;
+
+    public function districts(): HasMany
+    {
+        return $this->hasMany(District::class);
+    }
 }
